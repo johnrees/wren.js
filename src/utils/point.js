8caee441b46f20b7bacc30fc29c3e0fb7cc524d1
John Rees <john@bitsushi.com>
2017-09-21 10:31:11 +0100

    lint point

--- src/utils/point.js
+++ src/utils/point.js
@@ -1,33 +1,39 @@
-const _getXY = (start, end) => [end[0] - start[0], end[1] - start[1]]
+const _getXY = (start, end) => [end[0] - start[0], end[1] - start[1]];
 
 const length = (start, end) => {
-  const [x, y] = _getXY(start, end)
-  return Math.hypot(x, y)
-}
+  const [x, y] = _getXY(start, end);
+  return Math.hypot(x, y);
+};
 
 const pointOnLine = (distance, length) => (start, end) => {
-  const [x, y] = _getXY(start, end)
-  return [start[0] + x/length * distance, start[1] + y/length * distance]
-}
+  const [x, y] = _getXY(start, end);
+  return [start[0] + x / length * distance, start[1] + y / length * distance];
+};
 
 const percentageOnLine = (percentage = 0.5) => (start, end) => {
-  const [x, y] = _getXY(start, end)
+  const [x, y] = _getXY(start, end);
   return [start[0] + x * percentage, start[1] + y * percentage];
 };
 
-const rotateAroundPoint = ([originX, originY], angle=0) => ([pointX, pointY]) => {
+const rotateAroundPoint = ([originX, originY], angle = 0) => (
+  [pointX, pointY]
+) => {
   return [
-    Math.cos(angle) * (pointX-originX) - Math.sin(angle) * (pointY-originY) + originX,
-    Math.sin(angle) * (pointX-originX) + Math.cos(angle) * (pointY-originY) + originY
-  ]
-}
+    Math.cos(angle) * (pointX - originX) -
+      Math.sin(angle) * (pointY - originY) +
+      originX,
+    Math.sin(angle) * (pointX - originX) +
+      Math.cos(angle) * (pointY - originY) +
+      originY
+  ];
+};
 
 const angle = (start, end) => {
-  const [x, y] = _getXY(start, end)
+  const [x, y] = _getXY(start, end);
   return Math.atan2(y, x);
-}
+};
 
-const rad2deg = rads => (rads/Math.PI)*180
+const rad2deg = rads => rads / Math.PI * 180;
 
 const midpoint = percentageOnLine(0.5);
 
@@ -38,4 +44,4 @@ module.exports = {
   midpoint,
   rotateAroundPoint,
   rad2deg
-}
+};
