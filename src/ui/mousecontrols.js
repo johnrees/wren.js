a484d89440d245bdcb1024ecd7c9bd9215277ac6
John Rees <john@bitsushi.com>
2017-09-21 10:34:04 +0100

    moved mousecontrols

---
+++ src/ui/mousecontrols.js
@@ -0,0 +1,7 @@
+// function handleDragStart(event) {
+//   const circle = event.target;
+//   console.log(circle);
+// }
+// for (const circle of [...document.querySelectorAll("circle")]) {
+//   circle.addEventListener("mousedown", handleDragStart);
+// }
